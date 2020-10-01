f6108d978c98e8c7066e3ec855d81ec764736380
Julio César Echeverri Marulanda <julio.em7@gmail.com>
2020-10-01 02:11:09 -0500

    Se actualiza archivo de constantes para cambiar el color de la interfaz

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,9 +1,10 @@
 export const COLORS = {
-  PRIMARY: '#ef476f',
+  PRIMARY: '#007960',
+  _PRIMARY: '#ef476f',
   SECONDARY: '#01A280',
   TERTIARY: '#01CBA0',
   LIGHT: '#FFF',
-  GREY: '#EEE',
+  GREY: '#DDD',
   DARK: '#000',
   LINK: '#22555C',
 };
